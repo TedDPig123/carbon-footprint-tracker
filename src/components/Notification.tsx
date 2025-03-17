1eb49e9bd35951c8f7d1309afa100a355a8c8ff6
TedDPig_git <85619008+TedDPig123@users.noreply.github.com>
2025-03-17 11:56:44 -0400

    Adjusted the top margin of notification for better visibility

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -7,7 +7,7 @@ export default function Notification({notificationText} : {notificationText : st
 
     return(
         <div className='fixed h-[100%] w-[100%] flex justify-end items-start z-[0]'>
-            {showNotif && <div className = "z-10 mt-10.5 mr-3 relative w-[25%] min-w-[300px] rounded-[15px] bg-[linear-gradient(95deg,_#DCB0E4_-22.13%,_#320070_153.73%)] pl-[12px] pr-[12px] pt-[6px] pb-[10px] opacity-95 backdrop-blur-md">
+            {showNotif && <div className = "z-10 mt-22 mr-3 relative w-[25%] min-w-[300px] rounded-[15px] bg-[linear-gradient(95deg,_#DCB0E4_-22.13%,_#320070_153.73%)] pl-[12px] pr-[12px] pt-[6px] pb-[10px] opacity-95 backdrop-blur-md">
             <h1 className="z-3 text-[#022100] font-bold mb-[2px]">Daily Challenge!</h1>
             <p className="z-3 text-[#022100]">{notificationText}</p>
             <div className='cursor-pointer' onClick={()=>setShowNotif(e=>!e)}>
