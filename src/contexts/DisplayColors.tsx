a30d8dbeeb6d2432660682181810a86f1f2bc192
TedDPig_git <85619008+TedDPig123@users.noreply.github.com>
2025-03-18 20:58:44 -0400

    Establishing lightmode and darkmode styles

---
+++ src/contexts/DisplayColors.tsx
@@ -0,0 +1,11 @@
+export const lightModeStyles = {
+    headerText: "text-[#CDE4B0]",
+    gradientBg: "bg-[linear-gradient(95deg,_#CDE4B0_-22.13%,_#077000_153.73%)]",
+    innerText: "text-[#022100]",
+};
+
+export const darkModeStyles = {
+    headerText: "text-[#A4D4AE]",
+    gradientBg: "bg-[linear-gradient(95deg,_#1B3021_-22.13%,_#03400A_153.73%)]",
+    innerText: "text-[#D1F1DA]",
+};
