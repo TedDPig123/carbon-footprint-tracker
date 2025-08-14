f0b678623f4a98d535cc5d89a7e90b89da2691ce
TedDPig_git <85619008+TedDPig123@users.noreply.github.com>
2025-08-14 19:14:11 -0400

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,8 @@ import svgr from 'vite-plugin-svgr';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),svgr()],
-  base: "carbon-footprint-tracker",
+  base: "/carbon-footprint-tracker",
   build: {
-    outDir: 'build',
+    outDir: 'dist',
   },
 })
